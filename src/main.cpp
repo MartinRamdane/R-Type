17badd27eb0b7ec44ea7f2379aaedefb8eb15d3b
Ramdane Martin <martin.ramdane@epitech.eu>
2023-09-19 09:28:57 +0200

    [feature]: fix makefile and clean flies

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
-#include "Player.hpp"
-#include "server/Socket.hpp"
+
 int main()
 {
-    Player player;
-    player.setXPos(5);
-    std::cout << "player x pos: " << player.getXPos() << std::endl;
-    Socket socket;
-    socket.connect_to("127.0.0.1", 8080);
     return (0);
 }
