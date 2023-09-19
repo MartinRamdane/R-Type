a3234181be24e321f0e1e8619e5c1e02eed36759
Ramdane Martin <martin.ramdane@epitech.eu>
2023-09-19 11:35:09 +0200

    [feature]: Add Core Engine and other classes

---
+++ src/GameEngine/Core.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-MAR-5-1-rtype-martin.ramdane
+** File description:
+** Core
+*/
+
+#include "Core.hpp"
+
+Core::Core()
+{
+    _engine = std::make_shared<Engine>();
+    _game = std::make_shared<Game>(_engine);
+}
+
+Core::~Core()
+{
+}
+
+void Core::mainLoop()
+{
+    while (_engine->isRunning()) {
+        _engine->update("event");
+        _game->update();
+    }
+}
--- src/GameEngine/Engine.cpp
+++ src/GameEngine/Engine.cpp
@@ -9,8 +9,30 @@
 
 Engine::Engine()
 {
+    instance = this;
+    _isRunning = true;
 }
 
 Engine::~Engine()
 {
+    instance = nullptr;
+    _isRunning = false;
+}
+
+void Engine::update(std::string event)
+{
+    for (auto &collider : colliders)
+        collider.type1->collideAll(collider.type2, collider.squaredRadius, collider.func);
+    for (auto &entity : entities)
+        entity->updateAll();
+}
+
+void Engine::addEntityType(EntityType<IEntity> *type)
+{
+    entities.push_back(type);
+}
+
+void Engine::removeEntityType(EntityType<IEntity> *type)
+{
+    entities.remove(type);
 }
