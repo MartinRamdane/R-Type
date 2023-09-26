d4d7f6dc60610d0ef3104e8bc4aadf3d9018798f
gabrielmorais4 <gabriel.de-souza-morais@epitech.eu>
2023-09-26 10:53:21 +0200

    [feture] Json parsing

--- src/GameEngine/JsonParser.hpp
+++ src/GameEngine/JsonParser.hpp
@@ -7,7 +7,12 @@
 
 #pragma once
 
-#include <json/json.h>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 class JsonParser
 {
@@ -15,6 +20,48 @@ public:
     JsonParser();
     ~JsonParser();
 
-protected:
-private:
+    nlohmann::json readFile(std::string const &path)
+    {
+        std::ifstream inputFile(path);
+        return nlohmann::json::parse(inputFile);
+    }
+
+    std::vector<std::string> split(std::string const &str, char const delim) noexcept
+    {
+        std::vector<std::string> res = {};
+        std::size_t start{0};
+        std::size_t end{0};
+        while ((start = str.find_first_not_of(delim, end)) != std::string::npos)
+        {
+            end = str.find(delim, start);
+            res.push_back(str.substr(start, end - start));
+        }
+        return res;
+    }
+
+    template <typename T>
+    T get(nlohmann::json const jsonData, std::string const key)
+    {
+        int cou = count(key.begin(), key.end(), '.');
+        std::vector<std::string> keys = split(key, '.');
+
+        nlohmann::json const *leaf = &jsonData;
+
+        if (cou > 0)
+        {
+            for (int i = 0; i <= cou; i++)
+            {
+                if (leaf->contains(keys[i]))
+                {
+                    leaf = &leaf->at(keys[i]);
+                }
+                else
+                {
+                    std::cerr << "Key '" << keys[i] << "' not found!" << std::endl;
+                    return T();
+                }
+            }
+        }
+        return leaf->get<T>();
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
-#include "GameEngine/Core.hpp"
-#include "GameEngine/r-type/Player.hpp"
-#include "GameEngine/r-type/Enemy.hpp"
-#include "GameEngine/r-type/Projectile.hpp"
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "GameEngine/JsonParser.hpp"
 
 int main()
 {
-    // Core core;
-    // core.mainLoop();
+    // Ler o JSON do arquivo
+    JsonParser config;
+    nlohmann::json jsonData = config.readFile("../src/GameEngine/r-type/assets/Setup.json");
+
+    std::string title = config.get<std::string>(jsonData, "Game.Title");
+    // int width = config.get<int>(jsonData, "Game.Width");
+    // int height = config.get<int>(jsonData, "Game.Height");
+
+    std::cout << "Title: " << title << std::endl;
+    // std::cout << "Width: " << width << std::endl;
+    // std::cout << "Height: " << height << std::endl;
+
     return 0;
 }
