b534ac1d4347e8266bd06596784d6e9de3a0b7fa
Ramdane Martin <martin.ramdane@epitech.eu>
2023-10-09 13:17:48 +0200

    fix(Shield): postions

--- src/engine/r-type/Shield.cpp
+++ src/engine/r-type/Shield.cpp
@@ -60,8 +60,12 @@ void Shield::takeDamage(int damage)
 
 void Shield::update()
 {
-    if (_x != _oldX || _y != _oldY)
-        setOldPosition(_x, _y);
+    std::tuple<float, float> pos = getPosition();
+    std::tuple<float, float> oldPos = getOldPosition();
+    if (std::get<0>(pos) != std::get<0>(oldPos) || std::get<1>(pos) != std::get<1>(oldPos))
+    {
+        setOldPosition(std::get<0>(pos), std::get<1>(pos));
+    }
     if (_shieldIsUp)
     {
         auto pos = getPosition();
