0a7c7fb42c42946478e240a98dcb759e7c648dd0
Florian Grima <florian.grima@epitech.eu>
2023-10-23 14:03:00 +0200

    json parser bug

--- src/server/Instance.hpp
+++ src/server/Instance.hpp
@@ -7,40 +7,44 @@
 
 #ifndef HEADER_INSTANCE
 #define HEADER_INSTANCE
+#include <boost/asio.hpp>
 #include <iostream>
 #include <vector>
-#include <boost/asio.hpp>
-#include "UDPServer.hpp"
-#include "../global/ThreadPool.hpp"
 #include "../engine/Core.hpp"
+#include "../global/JsonParser.hpp"
+#include "../global/ThreadPool.hpp"
+#include "UDPServer.hpp"
 
 class ServerClass;
-class Instance
-{
-public:
-    Instance(int _id);
-    Instance(const Instance &) = delete;
-    ~Instance(); // destroy the instance if nbPlayers === 0
-    int getId() const { return _id; }
-    UDPServer *getUDPServer() { return _udpServer; }
-    void setServer(ServerClass *server) { _serverRef = server; }
-    int getNbPlayers() const { return _udpServer->getNbPlayers(); }
-    int getPort() const { return _port; }
-    Core *getCore() { return _core; }
-    void MessagesLoop();
-    void EventLoop();
-    void addAction(Event event) { _events.push_back(event); }
+class Instance {
+ public:
+  Instance(int _id, std::string gameName);
+  Instance(const Instance&) = delete;
+  ~Instance();  // destroy the instance if nbPlayers === 0
+  int getId() const { return _id; }
+  UDPServer* getUDPServer() { return _udpServer; }
+  void setServer(ServerClass* server) { _serverRef = server; }
+  int getNbPlayers() const { return _udpServer->getNbPlayers(); }
+  int getPort() const { return _port; }
+  Core* getCore() { return _core; }
+  void MessagesLoop();
+  void EventLoop();
+  void addAction(Event event) { _events.push_back(event); }
 
-protected:
-private:
-    int _id;
-    int _port;
-    boost::asio::io_service _io_service;
-    UDPServer *_udpServer;
-    ServerClass *_serverRef;
-    ThreadPool _threadPool;
-    Core *_core;
-    ThreadSafeQueue<Event> _events;
+ protected:
+ private:
+  int _id;
+  int _port;
+  int _nbPlayersMax;
+  bool _onlyMultiplayer;
+  std::string _gameName;
+  boost::asio::io_service _io_service;
+  UDPServer* _udpServer;
+  ServerClass* _serverRef;
+  ThreadPool _threadPool;
+  Core* _core;
+  ThreadSafeQueue<Event> _events;
+  JsonParser _jsonParser;
 };
 
 #endif /* !HEADER_INSTANCE */
--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -8,47 +8,39 @@
 #include "Server.hpp"
 #include "ServerImpl.hpp"
 
-ServerClass::ServerClass() : _threadPool(1)
-{
-    MyServer *server = new MyServer(4244, this);
-    _server = server;
-    _server->StartServer();
-    _playerIdToGive = 1;
+ServerClass::ServerClass() : _threadPool(1) {
+  MyServer* server = new MyServer(4244, this);
+  _server = server;
+  _server->StartServer();
+  _playerIdToGive = 1;
 }
 
-ServerClass::~ServerClass()
-{
-    _server->StopServer();
+ServerClass::~ServerClass() {
+  _server->StopServer();
 }
 
-InstanceInfos ServerClass::createInstance()
-{
-    Instance *instance = new Instance(_instances.size());
-    instance->setServer(this);
-    _instances.push_back(instance);
-    InstanceInfos instanceinfos;
-    instanceinfos.id = instance->getId();
-    instanceinfos.port = instance->getPort();
-    return instanceinfos;
+InstanceInfos ServerClass::createInstance() {
+  Instance* instance = new Instance(_instances.size(), "rtype");
+  instance->setServer(this);
+  _instances.push_back(instance);
+  InstanceInfos instanceinfos;
+  instanceinfos.id = instance->getId();
+  instanceinfos.port = instance->getPort();
+  return instanceinfos;
 }
 
-void ServerClass::interpretEvent(Event &event)
-{
-    if (event.ACTION_NAME == ACTION::CREATE)
-    {
-        std::cout << "CREATE" << std::endl;
-    }
+void ServerClass::interpretEvent(Event& event) {
+  if (event.ACTION_NAME == ACTION::CREATE) {
+    std::cout << "CREATE" << std::endl;
+  }
 }
 
-int ServerClass::getPortInstance(int instanceId)
-{
-    return _instances[instanceId]->getPort();
+int ServerClass::getPortInstance(int instanceId) {
+  return _instances[instanceId]->getPort();
 }
 
-void ServerClass::loop()
-{
-    while (1)
-    {
-        _server->HandleMessages(-1, true);
-    }
+void ServerClass::loop() {
+  while (1) {
+    _server->HandleMessages(-1, true);
+  }
 }
