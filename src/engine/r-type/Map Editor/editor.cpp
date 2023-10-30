2b458aca5d40de7f2e6372d9d773c393a860b6e1
gabrielmorais4 <gabriel.de-souza-morais@epitech.eu>
2023-10-30 21:25:46 +0100

    start

--- src/engine/r-type/Map Editor/editor.cpp
+++
@@ -1,7 +0,0 @@
-/*
-** EPITECH PROJECT, 2023
-** r-type
-** File description:
-** editor
-*/
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,23 @@
 #include "engine/Core.hpp"
 #include "server/Server.hpp"
+#include "engine/r-type/map_editor/Editor.hpp"
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: ./server <game> (rType or pong)" << std::endl;
-        return 1;
-    } else {
-        if (std::string(argv[1]) != "rType" && std::string(argv[1]) != "pong") {
-            std::cerr << "Usage: ./server <game> (rType or pong)" << std::endl;
-            return 1;
-        }
-    }
-    try {
-        ServerClass server(argv[1]);
-        server.loop();
-    } catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
-    }
-    return 0;
+    Editor editor;
+    // if (argc != 2) {
+    //     std::cerr << "Usage: ./server <game> (rType or pong)" << std::endl;
+    //     return 1;
+    // } else {
+    //     if (std::string(argv[1]) != "rType" && std::string(argv[1]) != "pong") {
+    //         std::cerr << "Usage: ./server <game> (rType or pong)" << std::endl;
+    //         return 1;
+    //     }
+    // }
+    // try {
+    //     ServerClass server(argv[1]);
+    //     server.loop();
+    // } catch (std::exception& e) {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    // return 0;
 }
